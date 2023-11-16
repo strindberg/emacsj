a886ee8f0d24a503258834efd2730bb3aa03f838
GitHub Action <action@github.com>
2023-11-16 14:13:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/strindberg/emacsj/services/MyProjectService.kt
+++ src/main/kotlin/com/github/strindberg/emacsj/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.strindberg.emacsj.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.strindberg.emacsj.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
