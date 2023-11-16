72eb612b6a9d3fe8fd2e5b3d6c67feb70485340b
Jesper Holmberg <jesper.holmberg@gmail.se>
2023-11-16 15:44:47 +0100

    Remove unused template code

--- src/test/kotlin/com/github/strindberg/emacsj/MyPluginTest.kt
+++ src/test/kotlin/com/github/strindberg/emacsj/MyPluginTest.kt
@@ -1,14 +1,10 @@
 package com.github.strindberg.emacsj
 
 import com.intellij.ide.highlighter.XmlFileType
-import com.intellij.openapi.components.service
 import com.intellij.psi.xml.XmlFile
-import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import com.github.strindberg.emacsj.services.MyProjectService
 
-@TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
 
     fun testXMLFile() {
@@ -24,16 +20,4 @@ class MyPluginTest : BasePlatformTestCase() {
             assertEquals("bar", it.value.text)
         }
     }
-
-    fun testRename() {
-        myFixture.testRename("foo.xml", "foo_after.xml", "a2")
-    }
-
-    fun testProjectService() {
-        val projectService = project.service<MyProjectService>()
-
-        assertNotSame(projectService.getRandomNumber(), projectService.getRandomNumber())
-    }
-
-    override fun getTestDataPath() = "src/test/testData/rename"
 }
