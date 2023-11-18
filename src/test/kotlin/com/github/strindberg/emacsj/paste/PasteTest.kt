dcc7bb9d2968283479c43b4e11bb8387dc29c8b3
Jesper Holmberg <jesper.holmberg@gmail.se>
2023-11-18 12:06:28 +0100

    Make paste test more robust

--- src/test/kotlin/com/github/strindberg/emacsj/paste/PasteTest.kt
+++ src/test/kotlin/com/github/strindberg/emacsj/paste/PasteTest.kt
@@ -89,13 +89,13 @@ class PasteTest : BasePlatformTestCase() {
     fun `test Paste history is rotated`() {
         myFixture.configureByText(FILE, "foo<caret>")
         CopyPasteManager.getInstance().setContents(BasicTransferable("zed", null))
-        Thread.sleep(50)
+        Thread.sleep(100)
         CopyPasteManager.getInstance().setContents(BasicTransferable("baz", null)) // discarded duplicate
-        Thread.sleep(50)
+        Thread.sleep(100)
         CopyPasteManager.getInstance().setContents(BasicTransferable("bar", null))
-        Thread.sleep(50)
+        Thread.sleep(100)
         CopyPasteManager.getInstance().setContents(BasicTransferable("baz", null))
-        Thread.sleep(50)
+        Thread.sleep(100)
 
         myFixture.performEditorAction(ACTION_PASTE)
         Thread.sleep(100)
