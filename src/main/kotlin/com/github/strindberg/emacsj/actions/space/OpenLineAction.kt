7800643992de690ac8c3f77e6cb09c2e4b9599a8
Jesper Holmberg <jesper.holmberg@gmail.se>
2023-11-22 13:36:52 +0100

    Remove open line action

--- src/main/kotlin/com/github/strindberg/emacsj/actions/space/OpenLineAction.kt
+++
@@ -1,6 +0,0 @@
-package com.github.strindberg.emacsj.actions.space
-
-import com.github.strindberg.emacsj.space.OpenLineHandler
-import com.intellij.openapi.editor.actions.TextComponentEditorAction
-
-class OpenLineAction : TextComponentEditorAction(OpenLineHandler())
