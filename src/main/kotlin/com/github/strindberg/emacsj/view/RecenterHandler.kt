c936cf5b99d5c417d97efc1464365d08b018b20a
Jesper Holmberg <jesper.holmberg@gmail.se>
2024-01-06 08:58:08 +0100

    Improve Recenter command at start/end of document

--- src/main/kotlin/com/github/strindberg/emacsj/view/RecenterHandler.kt
+++ src/main/kotlin/com/github/strindberg/emacsj/view/RecenterHandler.kt
@@ -3,6 +3,7 @@ package com.github.strindberg.emacsj.view
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.editor.Caret
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.VisualPosition
 import com.intellij.openapi.editor.actionSystem.EditorActionHandler
 
 class RecenterHandler : EditorActionHandler() {
@@ -13,6 +14,7 @@ class RecenterHandler : EditorActionHandler() {
         val viewOffset = editor.scrollingModel.verticalScrollOffset
         val viewHeight = editor.scrollingModel.visibleArea.height
         val caretOffset = editor.visualPositionToXY(primary.visualPosition).y
+        val editorEnd = editor.visualPositionToXY(VisualPosition(editor.document.lineCount, 0)).y
 
         val scrollTop = caretOffset - editor.lineHeight
         val scrollMiddle = caretOffset - viewHeight / 2
@@ -22,7 +24,16 @@ class RecenterHandler : EditorActionHandler() {
             when (viewOffset) {
                 scrollMiddle -> scrollTop
                 scrollTop -> scrollBottom
-                else -> scrollMiddle
+                scrollBottom -> scrollMiddle
+                else -> {
+                    if (caretOffset < viewHeight / 2) {
+                        scrollTop
+                    } else if (editorEnd - caretOffset < viewHeight / 2) {
+                        scrollBottom
+                    } else {
+                        scrollMiddle
+                    }
+                }
             }
         )
     }
