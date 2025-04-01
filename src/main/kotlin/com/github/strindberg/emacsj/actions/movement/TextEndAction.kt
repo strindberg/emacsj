768445598715c4f851c0d984d623612e8ea63df4
Jesper Holmberg <jesper.holmberg@gmail.se>
2025-04-01 08:43:05 +0200

    Add first versions of text movement commands

---
+++ src/main/kotlin/com/github/strindberg/emacsj/actions/movement/TextEndAction.kt
@@ -0,0 +1,7 @@
+package com.github.strindberg.emacsj.actions.movement
+
+import com.github.strindberg.emacsj.movement.MovementType
+import com.github.strindberg.emacsj.movement.TextMovementHandler
+import com.intellij.openapi.editor.actions.TextComponentEditorAction
+
+class TextEndAction : TextComponentEditorAction(TextMovementHandler(MovementType.END))
