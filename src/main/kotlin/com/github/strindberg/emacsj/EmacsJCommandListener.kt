c1ee2cabfc47a9a43de0a4a0cef01e283369a4ba
Jesper Holmberg <jesper.holmberg@gmail.se>
2025-05-02 08:39:50 +0200

    Move Xref history to proper handler

--- src/main/kotlin/com/github/strindberg/emacsj/EmacsJCommandListener.kt
+++ src/main/kotlin/com/github/strindberg/emacsj/EmacsJCommandListener.kt
@@ -1,28 +1,20 @@
 package com.github.strindberg.emacsj
 
-import com.github.strindberg.emacsj.mark.LimitedStack
-import com.github.strindberg.emacsj.mark.MarkHandler
-import com.github.strindberg.emacsj.mark.PlaceInfo
+import com.github.strindberg.emacsj.xref.XRefHandler
 import com.intellij.idea.ActionsBundle
 import com.intellij.openapi.command.CommandEvent
 import com.intellij.openapi.command.CommandListener
-import com.intellij.openapi.fileEditor.TextEditor
-import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
 
 class EmacsJCommandListener : CommandListener {
 
     companion object {
-        private val places = LimitedStack<PlaceInfo>()
-
         internal var lastCommandName: String? = null
             private set
-
-        internal fun popPlace(): PlaceInfo? = places.pop()
     }
 
     override fun commandFinished(event: CommandEvent) {
         // Empty or "Undefined" commands are present when running tests
-        if (event.commandName != "" && event.commandName != "Undefined") {
+        if (event.commandName.isNotBlank() && event.commandName != "Undefined") {
             lastCommandName = event.commandName
         }
     }
@@ -34,19 +26,7 @@ class EmacsJCommandListener : CommandListener {
                 ActionsBundle.message("action.GotoTypeDeclaration.text"),
             )
         ) {
-            event.project?.let { project ->
-                FileEditorManagerEx.getInstanceExIfCreated(project)?.let { fileEditorManager ->
-                    fileEditorManager.currentFile?.let { virtualFile ->
-                        fileEditorManager.getSelectedEditor(virtualFile)?.let { fileEditor ->
-                            (fileEditor as? TextEditor)?.editor?.let { editor ->
-                                MarkHandler.placeInfo(editor, virtualFile)?.let {
-                                    places.push(it)
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            XRefHandler.pushPlace(event)
         }
     }
 }
--- src/main/kotlin/com/github/strindberg/emacsj/xref/XRefHandler.kt
+++ src/main/kotlin/com/github/strindberg/emacsj/xref/XRefHandler.kt
@@ -1,20 +1,44 @@
 package com.github.strindberg.emacsj.xref
 
-import com.github.strindberg.emacsj.EmacsJCommandListener
+import com.github.strindberg.emacsj.mark.LimitedStack
 import com.github.strindberg.emacsj.mark.MarkHandler
+import com.github.strindberg.emacsj.mark.PlaceInfo
 import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.command.CommandEvent
 import com.intellij.openapi.editor.Caret
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.actionSystem.EditorActionHandler
 import com.intellij.openapi.editor.ex.EditorEx
+import com.intellij.openapi.fileEditor.TextEditor
+import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
 
 internal const val ACTION_XREF_BACK = "com.github.strindberg.emacsj.actions.xref.xrefback"
 
 class XRefHandler : EditorActionHandler() {
 
+    companion object {
+        private val places = LimitedStack<PlaceInfo>()
+
+        internal fun pushPlace(event: CommandEvent) {
+            event.project?.let { project ->
+                FileEditorManagerEx.getInstanceExIfCreated(project)?.let { fileEditorManager ->
+                    fileEditorManager.currentFile?.let { virtualFile ->
+                        fileEditorManager.getSelectedEditor(virtualFile)?.let { fileEditor ->
+                            (fileEditor as? TextEditor)?.editor?.let { editor ->
+                                MarkHandler.placeInfo(editor, virtualFile)?.let {
+                                    places.push(it)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     override fun doExecute(editor: Editor, caret: Caret?, dataContext: DataContext) {
         (editor as? EditorEx)?.let { ex ->
-            EmacsJCommandListener.popPlace()?.let { place ->
+            places.pop()?.let { place ->
                 MarkHandler.gotoPlaceInfo(editor, place)
             }
         }
