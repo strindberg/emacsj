f43e234765fc62d429fe730f1ae5ac3fe1f4fd83
Jesper Holmberg <jesper.holmberg@gmail.se>
2025-05-11 15:45:09 +0200

    Add some tests and constants

--- src/main/kotlin/com/github/strindberg/emacsj/search/ReplacePreviousHandler.kt
+++ src/main/kotlin/com/github/strindberg/emacsj/search/ReplacePreviousHandler.kt
@@ -9,6 +9,9 @@ import org.intellij.lang.annotations.Language
 @Language("devkit-action-id")
 internal const val ACTION_REPLACE_PREVIOUS = "com.github.strindberg.emacsj.actions.search.replaceprevious"
 
+@Language("devkit-action-id")
+internal const val ACTION_REPLACE_NEXT = "com.github.strindberg.emacsj.actions.search.replacenext"
+
 class ReplacePreviousHandler(private val forward: Boolean) : EditorActionHandler() {
 
     override fun doExecute(editor: Editor, caret: Caret?, dataContext: DataContext) {
