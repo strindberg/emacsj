383cd135d50e0746be40c151ba7b2562b97a4dee
Jesper Holmberg <jesper.holmberg@gmail.se>
2025-05-11 16:10:40 +0200

    Add XRef test

--- src/test/kotlin/com/github/strindberg/emacsj/xref/XRefTest.kt
+++ src/test/kotlin/com/github/strindberg/emacsj/xref/XRefTest.kt
@@ -1,13 +1,14 @@
 package com.github.strindberg.emacsj.xref
 
 import com.intellij.openapi.actionSystem.IdeActions.ACTION_GOTO_DECLARATION
+import com.intellij.openapi.actionSystem.IdeActions.ACTION_GOTO_TYPE_DECLARATION
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 
 const val FILE = "MyClass.kt"
 
 class XRefTest : BasePlatformTestCase() {
 
-    fun `test XRef back works`() {
+    fun `test XRef back works after Go to declaration`() {
         myFixture.configureByText(
             FILE,
             """
@@ -45,6 +46,44 @@ class XRefTest : BasePlatformTestCase() {
                     }
                     fun hello() {
                         println("Hello world!")
+                    }
+                }
+            """.trimIndent()
+        )
+    }
+
+    fun `test XRef back works after Go to type declaration`() {
+        myFixture.configureByText(
+            FILE,
+            """
+                class AClass {}
+                class MyClass {
+                    fun hello() {
+                        val a = A<caret>Class()
+                    }
+                }
+            """.trimIndent()
+        )
+
+        myFixture.performEditorAction(ACTION_GOTO_TYPE_DECLARATION)
+        myFixture.checkResult(
+            """
+                class <caret>AClass {}
+                class MyClass {
+                    fun hello() {
+                        val a = AClass()
+                    }
+                }
+            """.trimIndent()
+        )
+
+        myFixture.performEditorAction(ACTION_XREF_BACK)
+        myFixture.checkResult(
+            """
+                class AClass {}
+                class MyClass {
+                    fun hello() {
+                        val a = A<caret>Class()
                     }
                 }
             """.trimIndent()
