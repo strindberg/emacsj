fae9ed5d2a977d75d2cab76531fcede2166bf2c7
Kyle Waldner <kyle.waldner@Kyle-Waldner-LXWVMMXFX9.local>
2025-09-14 17:26:10 -0400

    forgot to commit these

---
+++ src/main/kotlin/com/github/strindberg/emacsj/actions/xref/XRefPushAction.kt
@@ -0,0 +1,7 @@
+package com.github.strindberg.emacsj.actions.xref
+
+import com.github.strindberg.emacsj.xref.XRefHandler
+import com.github.strindberg.emacsj.xref.XRefType
+import com.intellij.openapi.editor.actions.TextComponentEditorAction
+
+class XRefPushAction : TextComponentEditorAction(XRefHandler(XRefType.PUSH))
